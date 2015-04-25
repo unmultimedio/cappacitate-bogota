98b8bf0bffbea31c4c634c54a35f147fb2cdd951
Julian R Figueroa <julian@Julians-MacBook-Pro.local>
2015-04-25 11:31:54 -0500

    quinta sesion

--- CappacitateBogota/fragmentoslistas/src/main/java/net/sikuani/fragmentoslistas/Receiver.java
+++ CappacitateBogota/fragmentoslistas/src/main/java/net/sikuani/fragmentoslistas/Receiver.java
@@ -4,11 +4,14 @@ package net.sikuani.fragmentoslistas;
 import android.app.Activity;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +46,38 @@ public class Receiver extends Fragment {
         );
 
         lv.setAdapter(myAdapter);
+
+        registerForContextMenu(lv);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        myParentActivity.getMenuInflater()
+                .inflate(R.menu.list_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(myParentActivity,
+                        "Editando...",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item2:
+                Toast.makeText(myParentActivity,
+                        "Eliminando...",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item3:
+                Toast.makeText(myParentActivity,
+                        "Llamando...",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 
     @Override
